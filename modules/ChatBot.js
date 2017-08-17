3155a91d91b0168a514c3b25bf2058db83f0389c
Erlend Ellingsen <erlend.ame@gmail.com>
2017-08-18 01:11:48 +0200

    Added toLowerCase on input command check

--- modules/ChatBot.js
+++ modules/ChatBot.js
@@ -67,7 +67,7 @@ module.exports = function(receiver, body, config, req, res) {
 
         var response = null;
 
-        if (self.body.indexOf('tider ') != -1) {
+        if (self.body.toLowerCase().indexOf('tider ') != -1) {
             response = self.replies.openingHours();
         }
 
