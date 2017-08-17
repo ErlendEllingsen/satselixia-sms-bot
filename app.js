f84406f2bc17aefa65e3b7249832517fc9c3ef22
Erlend Ellingsen <erlend.ame@gmail.com>
2017-08-17 22:03:03 +0200

    Add body-parser

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
 
 app.post('/inbound', function (req, res) {
-    res.send('Hello World (post)');
+    res.send('Hello World (post)')
+    fs.writeFileSync('body.json', JSON.stringify(req.body));
 });
 
 app.listen(60000);
